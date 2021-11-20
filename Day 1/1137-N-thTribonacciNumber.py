4be2554ebb572f3aaa1791b2e35b65e6c2e0cdb5
Md Abdur Raiyan <raiyan1698@gmail.com>
2021-11-20 18:15:03 +0600

    Update 1137-N-thTribonacciNumber.py

--- Day 1/1137-N-thTribonacciNumber.py
+++ Day 1/1137-N-thTribonacciNumber.py
@@ -1,14 +1,11 @@
 class Solution:
     def __init__(self):
         self.dp = []
-        for i in range(40):
-            if i == 0:
-                self.dp.append(0)
-            elif i == 1 or i == 2:
-                self.dp.append(1)
-            else:
-                self.dp.append(self.dp[i-1] + self.dp[i-2] + self.dp[i-3])
-    
-    def tribonacci(self, n: int) -> int:
-        return self.dp[n]
+        self.dp.append(1)
+        self.dp.append(1)
+        for i in range(2, 45+1):
+            self.dp.append(self.dp[i-1] + self.dp[i-2])
         
+        
+    def climbStairs(self, n: int) -> int:
+        return self.dp[n]
